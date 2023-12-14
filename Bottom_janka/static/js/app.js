0fe1a52a6515bff55f35da41beaecc19e11f2d52
FIRST_NAME LAST_NAME <jankaglenn@gmail.com>
2023-12-14 15:17:38 -0500

    Janka Complete

--- Bottom_janka/static/js/app.js
+++ Bottom_janka/static/js/app.js
@@ -11,6 +11,9 @@ const renderCharts = () => {
         
         filteredData = data.find(obj => obj.Country == choice);
 
+        filteredData_2 = data.filter(obj => obj.Country == choice);
+        console.log(filteredData_2)
+
         console.log(filteredData);
 
         // metadata
@@ -23,31 +26,30 @@ const renderCharts = () => {
         var data = [{
             type: 'scattergeo',
             mode: 'markers',
-            // locations: ['FRA', 'DEU', 'RUS', 'ESP'],
-            locations: ['usa'],
+            locations: ['ESP','GRC','DEU','RUS','LBR','ITA','AUS','USA','JPN','CHE','TCD','BRA'],
             marker: {
-                size: [20, 30, 15, 10],
-                color: [10, 20, 40, 50],
-                cmin: 0,
-                cmax: 50,
-                colorscale: 'Greens',
+                size: [5, 5, 5, 25, 25, 15, 25, 35, 15, 15, 20, 20, 25],
+                color: [15, 5, 20, 50, 50, 30, 20, 20, 20, 25, 60, 40],
+                cmin: 70,
+                cmax: 10,
+                colorscale: 'Blues',
                 colorbar: {
-                    title: 'Some rate',
-                    ticksuffix: '%',
-                    showticksuffix: 'last'
+                    title: 'YEARS',
+                    ticksuffix: '0',
+                    showticksuffix: 'first'
                 },
                 line: {
                     color: 'black'
                 }
             },
-            name: 'europe data'
+            name: 'Blue Zones'
         }];
 
         var layout = {
             'geo': {
                 // 'scope': 'europe',
                 // 'scope': 'france',
-                'resolution': 500,
+                // 'resolution': 500,
 
             }
         };
@@ -77,7 +79,7 @@ const url = "https://raw.githubusercontent.com/bkeffer3098/Project-3/main/LifeEx
 
 d3.json(url).then(data => {
     x = data;
-
+console.log(data)
     data.map(obj => obj.Country).forEach(loc => {
         d3.select('select').append('option').text(loc);
     });
