a83a307d52090c0139e69469ab1b7d78327e22e4
bkeffer3098 <bkeffer3098@gmail.com>
2023-12-17 18:28:10 -0500

    title change

--- Map/static/js/map.js
+++ Map/static/js/map.js
@@ -10,11 +10,22 @@ let myMap = L.map("map", {
   }).addTo(myMap);
   
 // Load the JSON data.
-  let url = "https://raw.githubusercontent.com/bkeffer3098/Project-3/main/Resources/Final%20Data/HALE_60_2019_Both.json";
+  let url = "https://raw.githubusercontent.com/bkeffer3098/Project-3/main/Resources/Final%20Data/male_female.json";
   
 let countryMarkers = [];
+let countryMale = [];
+let countryFemale = [];
 let countryLocation = [];
 let countryBoth = [];
+
+let greenIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
 
 d3.json(url).then(function (data) {
   console.log(data);
@@ -22,33 +33,25 @@ d3.json(url).then(function (data) {
   // Initial Map Layer
   for (let i = 0; i < data.length; i++) {
     try {
-      countryLocation.push([Number(data[i].Latitude), Number(data[i].Longitude)]);
-      countryMarkers.push(
-      L.marker(countryLocation[i]).bindPopup("<h1>" + data[i].Country + "</h1>" + "<h3>" + "Average years to live after age 60: </h3>" + "<h3>" + data[i]["Age Expectancy"] + " years </h3>").addTo(myMap)
+      if (data[i].Sex) {
+        countryLocation.push([Number(data[i].Latitude), Number(data[i].Longitude)]);
+        countryMarkers.push(
+        L.marker(countryLocation[i],{icon: greenIcon}).bindPopup("<h1>" + data[i].Country + "</h1>" + "<h3>" + "Average years to live after age 60: </h3>" + "<h3>" + data[i]["Age Expectancy"] + " years </h3>")
       );}
+      }
     catch(err){}
   }
   
-  // Layer centered around Both Sexes
-  // for (let i = 0; i < data.length; i++) {
-  //   if (data[i].HALE == "Healthy life expectancy (HALE) at birth (years)")
-  //   countryBoth.push(
-  //     L.marker(countryLocation[i]).bindPopup("<h1>" + data[i].Country + "</h1>" + "<h2>" + "Age Expectancy: " + data[i]["Age Expectancy"] + "</h2>").addTo(myMap)
-  //   );}
+  let bothLayer = L.layerGroup(countryMarkers);
 
-  //   console.log(countryBoth);
-    });
+  let baseMaps = {
+    "Original View": canvas
+  };
   
-// let bothLayer = L.layerGroup(countryBoth);
-
-let baseMaps = {
-  "Original View": canvas
-};
-
-// Overlays that can be toggled on or off
-let overlayMaps = {
+  // Overlays that can be toggled on or off
+  let overlayMaps = {
+    "Both Sexes": bothLayer
+  };
   
-};
-
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+});
